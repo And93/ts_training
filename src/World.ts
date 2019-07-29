6e82bc54946aec5e13ff38eeddca6ee68c99ba3d
Andrei Dzeichyk <andreidei4ik@gmail.com>
2019-07-30 00:02:06 +0300

    Changes:
    - added class generic for the getPage function;
    - cs fix;
    - upd dependencies.

--- src/World.ts
+++ src/World.ts
@@ -1,26 +1,32 @@
-import {Events} from "./pages/Events";
-import {Themes} from "./pages/Themes";
-import {News} from "./pages/News";
-import {Page} from "./pages/Page";
-
-import {IEvents, INews, IThemes} from './pages/Main';
-
-const events = new Events("events");
-const themes = new Themes("themes");
-const news = new News("news");
+import {Events} from './pages/Events';
+import {Themes} from './pages/Themes';
+import {Page} from './pages/Page';
+import {News} from './pages/News';
 
 export class World {
-    public getPage(name: string): Page {
-        if (name === IEvents.name) {
-            return events;
-        } else if (name === IThemes.name) {
-            return themes;
-        } else if (name === INews.name) {
-            return news;
-        }
+    public getPage<T extends Page>(page: new (name: string) => T): T {
+
+        const pages: { [key: string]: any } = {
+            events: Events,
+            themes: Themes,
+            'news': News
+        };
+
+        const _page = pages[page.name.toLowerCase()];
+        return new _page(page.name);
+
+        // return new page(page.name);
+    }
+
+    public gotPage<T>(page: { new(name: string): T; }): T {
+        return new page(page.name);
     }
 }
 
 const world = new World();
 
-console.log(world.getPage('Themes').foo());
+world.getPage(Themes).foo();
+world.gotPage(Events).foo();
+
+console.log(world.getPage(News).constructor.name);
+
